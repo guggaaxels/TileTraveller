07fead9ddba91c9b28335d5d0066f9ad4a61b548
Gugga Axels <guggaaxels@Guggas-MacBook-Pro-2.local>
2018-09-18 10:24:58 +0000

    inital work

--- TileTraveller.py
+++ TileTraveller.py
@@ -4,3 +4,11 @@ y_start = 1
 victory = False
 print("You can travel (N)orth. ")
 direction = str(input("Direction: "))
+
+if direction == "n" or direction == "N":
+    x_start = x_start
+    y_start += 1
+    print("You can travel: (N)orth or (E)ast or (S)outh. ")
+else:
+    print("Not a valid direction")
+    
